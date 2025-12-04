4e1c78a57d63a11314e900a2693444161b45c654
mohamedsaleh077 <mohammed.saleh.22707@gmail.com>
2025-12-05 00:08:11 +0200

    adding projects

--- script.js
+++ script.js
@@ -16,33 +16,69 @@ function toggle_window(id) {
     $(id + '-task').toggle();
 }
 
-function mini_window(id) {
-    $(id).toggle();
-}
 
 updateTime();
 
+function increasingProgressBar() {
+    let currentValue = parseFloat($(".progress-bar").val());
+    if (currentValue < 0.99) {
+        let newValue = currentValue + 0.01;
+        $(".progress-bar").val(newValue)
+    }
+}
+
+function getGitHubRepos() {
+
+}
 
 $(document).ready(function () {
     setInterval(updateTime, 50000);
 
     // about me section
-    $("#aboutme").hide();
-    $("#aboutme-task").hide();
+    $("#aboutme-icon, #aboutme-okay, #close-aboutme").click(function () {
+        toggle_window("#aboutme");
+    });
 
-    $("#aboutme-icon").click(function () {
-        toggle_window("#aboutme")
-    })
+    $("#mini-aboutme, #aboutme-task").click(function () {
+        $("#aboutme").toggle();
+    });
 
-    $("#close-aboutme").click(function () {
-        toggle_window("#aboutme")
-    })
+    let gettingRequestInterval
+    // My Projects
+    $("#myprojects-icon, #myprojecst-okay, #close-myprojects").click(function () {
+        if ($("#myproject").is(":visible")) {
+            clearInterval(gettingRequestInterval);
+        } else {
+            gettingRequestInterval = setInterval(increasingProgressBar, 100);
+            $.get("https://api.github.com/users/mohamedsaleh077/repos", function (data, status) {
+                console.log("Data: " + data + "\nStatus: " + status);
 
-    $("#mini-aboutme").click(function () {
-        mini_window("#aboutme")
-    })
+                let reposHTML = ``
+                data.forEach(element => {
+                    if (element.private !== true) {
+                        reposHTML += `
+                            <h3>${element.name}</h3>
+                            <p>${element.description}</p>
+                            <a href="${element.html_url}" target="_blank"><button>Visit</button></a>
+                            <br><hr><br>
+                        `;
+                    }
+                });
+                $("#project").html(reposHTML);
 
-    $("#aboutme-task").click(function () {
-        mini_window("#aboutme")
-    })
+                if (status === "success") {
+                    clearInterval(gettingRequestInterval);
+                    $("#loading").hide();
+                }
+
+            });
+        }
+        toggle_window("#myprojects");
+    });
+
+    $("#mini-myprojects, #myprojects-task").click(function () {
+        $("#myprojects").toggle();
+    });
+
+
 });
